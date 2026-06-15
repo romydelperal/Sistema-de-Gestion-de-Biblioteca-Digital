85715198576b98338c6d8bfc41c9b8558282c0b6
romydelperal <romydelperal2606@gmail.com>
2026-06-15 17:04:00 -0300

    Update singletonmeta.py

--- singletonmeta.py
+++ singletonmeta.py
@@ -1,8 +1,8 @@
-# 1. METACLASE: Implementa el patrón Singleton para asegurar una única instancia de la Biblioteca
-class SingletonMeta(type): # Metaclase que hereda de 'type' [3]
-    _instances = {} # Diccionario para almacenar la instancia única
-    def __call__(cls, *args, **kwargs): # Controla la creación de la clase
-        if cls not in cls._instances: # Si no existe la instancia, la crea
-            cls._instances[cls] = super().__call__(*args, **kwargs) # Almacena la instancia
-        return cls._instances[cls] # Retorna siempre la misma instancia
+# METACLASE: Implementa el patrón Singleton para asegurar una única instancia de la Biblioteca
+class SingletonMeta(type):  # Metaclase que hereda de 'type' 
+    _instances = {}  # Diccionario para almacenar la instancia única
+    def __call__(cls, *args, **kwargs):  # Controla la creación de la clase
+        if cls not in cls._instances:  # Si no existe la instancia, la crea
+            cls._instances[cls] = super().__call__(*args, **kwargs)  # Almacena la instancia
+        return cls._instances[cls]  # Retorna siempre la misma instancia
     
