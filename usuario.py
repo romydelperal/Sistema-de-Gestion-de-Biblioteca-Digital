7c72bfef5ed081a74f00fbbb3224d65a549c1fd0
romydelperal <romydelperal2606@gmail.com>
2026-06-15 17:15:21 -0300

    Update usuario.py

--- usuario.py
+++ usuario.py
@@ -1,7 +1,7 @@
 from persona import Persona
 from cuenta import Cuenta
 class Usuario(Persona):  # Clase Usuario que hereda de Persona (Jerarquía de Herencia) 
-    def __init__(self, nombre, apellido, dni, correo_electronico): # Constructor con datos completos
+    def __init__(self, nombre, apellido, dni, correo_electronico):  # Constructor con datos completos
         super().__init__(nombre, apellido, dni)  # Llama al constructor del padre (Persona) 
         # COMPOSICIÓN: Se crea una instancia de Cuenta que pertenece solo a este Usuario 
         self.cuenta = Cuenta(correo_electronico) 
