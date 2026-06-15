59b5bfba87e3a42849e8ab8bc4ea50d672d335fd
romydelperal <romydelperal2606@gmail.com>
2026-06-15 17:00:53 -0300

    Update cuenta.py

--- cuenta.py
+++ cuenta.py
@@ -1,5 +1,5 @@
-# 2. RELACIÓN DE COMPOSICIÓN: La existencia de Cuenta depende de Usuario [2]
+# 2. RELACIÓN DE COMPOSICIÓN: La existencia de Cuenta depende de Usuario 
 class Cuenta: # Clase que representa la cuenta de acceso del usuario
-    def __init__(self, email): # Constructor que recibe el correo electrónico [1]
-        self.email = email # Asignación del correo electrónico (dato mínimo) [1]
-        self.estado = "Activo" # Estado inicial de la cuenta dentro del sistema
+    def __init__(self, email):  # Constructor que recibe el correo electrónico 
+        self.email = email  # Asignación del correo electrónico (dato mínimo) 
+        self.estado = "Activo"  # Estado inicial de la cuenta dentro del sistema
