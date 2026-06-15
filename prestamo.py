356c33355e5ba69b254cb585ccbb5ba49004b5f5
romydelperal <romydelperal2606@gmail.com>
2026-06-15 17:14:51 -0300

    Update prestamo.py

--- prestamo.py
+++ prestamo.py
@@ -1,5 +1,5 @@
 class Prestamo:  #Clase que será parte de una composición en Biblioteca.
-    def __init__(self, usuario, libro, fecha_prestamo): # Constructor del registro de préstamo
+    def __init__(self, usuario, libro, fecha_prestamo):  # Constructor del registro de préstamo
         self.usuario = usuario
         self.libro = libro
         self.fecha_prestamo = fecha_prestamo
