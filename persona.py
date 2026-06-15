97789a15836ea9e5024e2e122e9e79a78b1fb1c6
romydelperal <romydelperal2606@gmail.com>
2026-06-15 17:03:17 -0300

    Update persona.py

--- persona.py
+++ persona.py
@@ -1,10 +1,10 @@
 from metavalidadora import MetaValidadora
 # JERARQUÍA DE HERENCIA: Persona como clase base 
-class Persona(metaclass=MetaValidadora): # Clase base que utiliza la metaclase definida
-    def __init__(self, nombre, apellido, dni): # Constructor con los datos personales básicos [1]
-        self.nombre = nombre # Almacena el nombre del usuario [1]
-        self.apellido = apellido # Almacena el apellido del usuario [1]
-        self.dni = dni # Almacena el DNI, identificador clave para el CRUD [1]
+class Persona(metaclass=MetaValidadora):  # Clase base que utiliza la metaclase definida
+    def __init__(self, nombre, apellido, dni):  # Constructor con los datos personales básicos 
+        self.nombre = nombre  
+        self.apellido = apellido  
+        self.dni = dni 
 
-    def mostrar_perfil(self): # Método base para ser sobreescrito (Polimorfismo) [2]
-        return f"{self.apellido}, {self.nombre} (DNI: {self.dni})" # Retorna formato estándar de nombre
+    def mostrar_perfil(self):  # Método base para ser sobreescrito (Polimorfismo) 
+        return f"{self.apellido}, {self.nombre} (DNI: {self.dni})"  # Retorna formato estándar de nombre
